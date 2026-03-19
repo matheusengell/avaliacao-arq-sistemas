3829d60fb0074941a8deb3aba1bca4d650cec452
matheus engel <matheusengelfl@gmail.com>
2026-03-19 15:12:41 -0300

    first commit

---
+++ src/main/java/com/example/prova_arquitetura/ProvaArquiteturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.prova_arquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaArquiteturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaArquiteturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prova_arquitetura/ProvaArquiteturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prova_arquitetura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaArquiteturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
